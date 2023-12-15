149de334f47d60f0e671648c554f0c558b860d3f
josebeo2016 <josebeo2016@gmail.com>
2023-12-15 13:01:56 +0900

    fix bug

--- systems/__init__.py
+++ systems/__init__.py
@@ -1,7 +1,7 @@
 
-from .aasistsslori import aasist_ssl_detect
-from .vocosigsept1 import vocosig_detect
-from .btse import btse_detect as btse_wav2vec_detect
+from .aasistssl_nov22 import aasist_ssl_detect
+from .vocosig_nov22 import vocosig_detect
+from .btse_nov22 import btse_detect as btse_wav2vec_detect
 
 SUPPORT_CM = ['aasist_ssl', 'vocosig', 'btse_wav2vec']
 
