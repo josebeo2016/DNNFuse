1d65a400f1cc0b085758b4ec871d34ed3016ffe6
josebeo2016 <josebeo2016@gmail.com>
2024-01-22 16:13:21 +0900

    update vocosig

--- systems/config.py
+++ systems/config.py
@@ -1,5 +1,5 @@
 BTSE_CONFIG = {
-    'version': 'D-2311_M-BTSE-3',
+    'version': 'D-2312_M-BTSE-3',
     'model_path': '/dataa/phucdt/DNNFuse/systems/btse_model/models/model_weighted_CCE_100_8_1e-06_supcon_nov22_finetune_new_Trans_64concat/epoch_14.pth',
     'config_path': '/dataa/phucdt/DNNFuse/systems/btse_model/configs/model_config_RawNet_Trans_64concat.yaml',
     'threshold': -7.82,
@@ -7,14 +7,14 @@ BTSE_CONFIG = {
 }
 
 AASISTSSL_CONFIG = {
-    'version': 'D2311_M-AASIST-SSL',
+    'version': 'D2312_M-AASIST-SSL',
     'model_path': '/dataa/phucdt/DNNFuse/systems/aasist_ssl/models/model_weighted_CCE_100_4_1e-06_supcon_nov22_new_finetune/epoch_47.pth',
     'threshold': 1.47,
     'enable': True,
 }
 
 CONFORMER_CONFIG = {
-    'version': 'D2311_M-AASIST-SSL',
+    'version': 'D2312_M-AASIST-SSL',
     'model_path': '/dataa/phucdt/DNNFuse/systems/aasist_ssl/models/model_weighted_CCE_100_10_1e-06_supcon_nov22_new_finetune_conformerer/epoch_23.pth',
     'threshold': 1.47,
     'enable': True,
@@ -23,7 +23,7 @@ CONFORMER_CONFIG = {
 
 VOCOSIG_CONFIG = {
     'version': 'D2312_M-VOCOSIG-5-R',
-    'model_path': '/dataa/phucdt/DNNFuse/systems/scl_vocosig/pretrained/conf-5-linear-jan10-r-finetune-epoch31.pth',
+    'model_path': '/dataa/phucdt/DNNFuse/systems/scl_vocosig/pretrained/conf-5-linear-finetune-from_nov22-29-epoch49.pth',
     'config_path': '/dataa/phucdt/DNNFuse/systems/scl_vocosig/configs/conf-5-linear.yaml',
     'threshold': -4.4781,
     'enable': True,
